af87ca063868c75f19dc2109e3df51326d0d3742
rn00n <rn00n@lezhin.com>
2025-07-01 16:16:34 +0900

    feat: password 버그수정

--- src/main/kotlin/com/rn00n/inhibitor/application/auth/usecase/login/LoginUseCase.kt
+++ src/main/kotlin/com/rn00n/inhibitor/application/auth/usecase/login/LoginUseCase.kt
@@ -1,8 +1,8 @@
 package com.rn00n.inhibitor.application.auth.usecase.login
 
 import co.elastic.apm.api.CaptureSpan
-import com.rn00n.inhibitor.application.auth.exception.ErrorCode
 import com.rn00n.inhibitor.application.auth.exception.DefaultOAuth2AuthenticationException
+import com.rn00n.inhibitor.application.auth.exception.ErrorCode
 import org.springframework.security.crypto.password.PasswordEncoder
 
 abstract class LoginUseCase(
@@ -11,7 +11,18 @@ abstract class LoginUseCase(
 
     @CaptureSpan("LoginUseCase#validatePassword")
     protected fun validatePassword(encodedPassword: String?, rawPassword: String?) {
-        if (encodedPassword.isNullOrBlank() || rawPassword.isNullOrBlank()) return
+        if (encodedPassword.isNullOrBlank()) {
+            throw DefaultOAuth2AuthenticationException(
+                ErrorCode.USER_INCORRECT_PASSWORD,
+                "encoded password is null or blank"
+            )
+        }
+        if (rawPassword.isNullOrBlank()) {
+            throw DefaultOAuth2AuthenticationException(
+                ErrorCode.USER_INCORRECT_PASSWORD,
+                "raw password is null or blank"
+            )
+        }
 
         if (!passwordEncoder.matches(rawPassword, encodedPassword)) {
             throw DefaultOAuth2AuthenticationException(
